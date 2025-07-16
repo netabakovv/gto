eda96a62a72e8442eb244aa76e12f5180ba1c156
netabakovv <netabakovv@yandex.ru>
2025-07-17 00:08:24 +0400

    delete frontend from repo

--- front/src/services/judgeService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API = "http://localhost:8080/api/judge";
-
-export const getMyEvents = () => axios.get(`${API}/my-events`);
-export const submitResult = (eventId, resultData) =>
-    axios.post(`${API}/${eventId}/submit-result`, resultData);
--- front/src/services/resultService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const API = "http://localhost:8080/api/results";
-
-export const getUserResults = (userId) =>
-    axios.get(`${API}/user/${userId}`);
